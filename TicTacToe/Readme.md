74b49aeddeb5c4f99d1c38914e01b4ea8d7deb0b
amanRathod <aksrathod07@gmail.com>
2023-04-13 15:35:06 +0530

    create tic-tac-toe

---
+++ TicTacToe/Readme.md
@@ -0,0 +1,7 @@
+# Tic-Tac-Toe
+
+### Objects
+1. Piece :- ```x```, ```O```, ``$``, ```#```.
+2. Board :- ```n*m```
+3. Player
+4. Game
---
+++ TicTacToe/src/Model/Board.java
@@ -0,0 +1,55 @@
+package Model;
+
+import jdk.internal.util.xml.impl.Pair;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Board {
+    public int size;
+    public PlayingPiece[][]board;
+
+    public Board(int size) {
+        this.size = size;
+        board = new PlayingPiece[size][size];
+    }
+
+    public boolean addPiece(int row, int column, PlayingPiece playingPiece) {
+
+        if(board[row][column] != null) {
+            return false;
+        }
+        board[row][column] = playingPiece;
+        return true;
+    }
+
+    public List<Pair> getFreeCells() {
+        List<Pair> freeCells = new ArrayList<>();
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (board[i][j] == null) {
+                    Pair rowColumn = new Pair();
+                    freeCells.add(rowColumn);
+                }
+            }
+        }
+
+        return freeCells;
+    }
+
+    public void printBoard() {
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (board[i][j] != null) {
+                    System.out.print(board[i][j].pieceType.name() + "   ");
+                } else {
+                    System.out.print("    ");
+                }
+                System.out.print(" | ");
+            }
+            System.out.println();
+        }
+    }
+}
